bbfbaeabc2085484c78c602cb3512ade70bbbd51
Samseppiol <j_hage@live.com>
2017-04-26 13:15:49 +1000

    Changed like synmbol to thumbs up

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,9 @@
 module PhotosHelper
+  def like_icon(liked)
+    if liked
+      '<i class="fa fa-thumbs-up fa-3x" aria-hidden="true"></i>'.html_safe
+    else
+      '<i class="fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i>'.html_safe
+    end
+  end
 end
